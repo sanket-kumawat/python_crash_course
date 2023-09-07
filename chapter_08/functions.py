d7086393e58de20abc9efc9c9b5424d5dfef30fd
Sanket Kumawat <kumawatsanket@gmail.com>
2023-09-07 11:12:24 +0530

    completed chapter_08

---
+++ chapter_08/functions.py
@@ -0,0 +1,61 @@
+def greet_user():
+    """Display a simple greeting."""
+    print("Hello!")
+
+
+greet_user()
+
+
+# Passing Information to a Function
+def greet(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+
+
+greet('sanket')
+
+
+# Keyword Arguments
+def describe_pet(animal_type, pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_pet(animal_type='hamster', pet_name='harry')
+
+
+# Default Values
+def describe_dog(pet_name, animal_type='dog'):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+
+describe_dog(pet_name='willie')
+
+
+# Passing an Arbitrary Number of Arguments
+
+def make_pizza(*toppings):
+    """Print the list of toppings that have been requested."""
+    print(toppings)
+
+
+make_pizza('pepperoni')
+make_pizza('mushrooms', 'green peppers', 'extra cheese')
+
+
+# Using Arbitrary Keyword Arguments
+
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+
+user_profile = build_profile('albert', 'einstein',
+                             location='princeton',
+                             field='physics')
+print(user_profile)
